814045cb5766610993594899d11a0f743b8daa44
NebulaBC <contact@nebulabc.net>
2022-06-22 23:13:45 -0400

    Final

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from datetime import datetime
+import re
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def index():
+    return render_template('index.html')
 
+@app.route('/', methods = ['POST'])
+def gettime():
+    date = datetime.strptime(request.form['datetime'], '%Y-%m-%dT%H:%M')
+    return render_template('index.html', timestamp="<br>Paste this code into discord for<br>a multi-timezone timestamp: &lt;t:" + str(datetime.timestamp(date)).split('.', 1)[0] + "&gt;")
 if __name__ == "__main__":
   app.run()
